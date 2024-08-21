416119ef22d075552087bd0942fc22fbc4031beb
Hellooitsme123 <studiosphantom.ml9@gmail.com>
2024-08-20 17:30:57 -0700

    the climax is only the beginning

---
+++ puzzle.js
@@ -0,0 +1,47 @@
+puzzles = {
+    "The Novice": {
+        problem1: "box[class='box-white',given='hello',answer='hi',pos='1:2']||box[class='box-navy',given='What is a synonym of hello?',pos='0:1]",
+        problem2: "box[class='box-white',given='sphere',answer='ball',pos='1:2']||box[class='box-navy',given='What is a synonym of sphere?',pos='0:1']",
+        problem3: "box[class='box-white',given='high',answer='hi',pos='1:0']||box[class='box-navy',given='What sounds exactly like high?',pos='0:0']",
+        problem4: "box[class='box-white',given='bawl',answer='ball',pos='1:0']||box[class='box-navy',given='Do you see whites pattern?',pos='0:0']",
+        problem5: "box[class='box-white',given='hi',answer='hi',pos='1:1']||box[class='box-navy',given='Each color has its own rule.',pos='0:1']",
+        problem6: "box[class='box-white',given='altitude',pos='1:2']||box[class='box-white',given='height',answer='height',pos='1:1']||box[class='box-navy',given='Height has its own rule too.',pos='0:1']",
+        problem7: "box[class='box-white',given='tow',answer='toe',pos='1:0']||box[class='box-navy',given='White is similarity.',pos='0:0']",
+        problem8: "box[class='box-white',given='okay',answer='okay',pos='1:1']||box[class='box-navy',given='Maybe try being on your own.',pos='0:1']",
+        problem9: "box[class='box-white',given='duplicate',answer='clone',pos='1:2']",
+        problem10: "box[class='box-white',given='wealthy',answer='rich',pos='1:2']",
+        problem11: "box[class='box-white',given='the novice',answer='the novice',pos='1:1']"
+    },
+    "The Colossal": {
+        problem1: "box[class='box-blue',given='h',answer='hi',pos='1:1']||box[class='box-navy',given='All color tutorials will start the same.',pos='0:1']",
+        problem2: "box[class='box-blue',given='all',answer='ball',pos='1:1']||box[class='box-navy',given='Find the rule based on the answers.',pos='0:1']",
+        problem3: "box[class='box-blue',given='may',answer='maybe',pos='1:1']||box[class='box-navy',given='Do you see the pattern?',pos='0:1']",
+        problem4: "box[class='box-blue',given='pull',answer='opal',pos='1:0']||box[class='box-navy',given='The answer is opal.',pos='0:0']",
+        problem5: "box[class='box-blue',given='kit',answer='bucket',pos='1:0']||box[class='box-navy',given='Add sounds.',pos='0:0']||box[class='box-navy',given='What word has the sound kit in it?',pos='2:0']",
+        problem6: "box[class='box-blue',given='raid',answer='radar',pos='1:0']||box[class='box-navy',given='The answer is similar to sonar.',pos='0:0']",
+        problem7: "box[class='box-blue,given='rad',answer='radar',pos='1:1']||box[class='box-navy',given='Now add letters.',pos='0:1']",
+        problem8: "box[class='box-blue',given='wheel',answer='car',pos='1:2']||box[class='box-navy',given='A wheel is in what?',pos='0:2']",
+        problem9: "box[class='box-blue',given='man',answer='human',pos='1:1']||box[class='box-blue',given='hue',pos='1:0']||box[class='box-blue',given='brain',pos='1:2']||box[class='box-navy',given='More blocks can provide hints.',pos='0:1']",
+        problem10: "box[class='box-blue',given='ight',answer='right',pos='1:1']||box[class='box-white',given='rite',pos='1:0']||box[class='box-navy',given='Enjoy it while you can.',pos='0:1']",
+        problem11: "box[class='box-blue',given='the loss',answer='the colossal',pos='1:1']||box[class='box-white',given='the ginormous',pos='1:2']||box[class='box-navy',given='Remember the rules.',pos='0:1']",
+
+    },
+    "The Firebrand": {
+        problem1: "box[class='box-red',given='high',answer='hi',pos='1:1']||box[class='box-navy',given='One more time.',pos='0:1']",
+        problem2: "box[class='box-red',given='football',answer='ball',pos='1:1']||box[class='box-navy',given='Do you remember?.',pos='0:1']",
+        problem3: "box[class='box-red',given='contemplation',answer='pain',pos='1:1']||box[class='box-navy',given='What does this question make you feel?.',pos='0:1']",
+        problem4: "box[class='box-red',given='car',answer='wheel',pos='1:2']||box[class='box-navy',given='Red is the opposite of blue.',pos='0:2']",
+        problem5: "box[class='box-red',given='room',answer='walls',pos='1:2']||box[class='box-navy',given='What is in a room?.',pos='0:2']",
+        problem6: "box[class='box-red',given='opal',answer='pull',pos='1:0']||box[class='box-navy',given='Remember this one?',pos='0:0']",
+        problem7: "box[class='box-red',given='finalize',answer='fine',pos='1:0']||box[class='box-navy',given='Remove sounds.',pos='0:0']",
+        problem8: "box[class='box-blue',given='par',answer='part',pos='1:0']||box[class='box-red',given='apartment',pos='1:1']||box[class='box-red',given='whole',pos='1:2']||box[class='box-navy',given='Red is subtraction. Blue is addition.',pos='0:1']",
+        problem9: "box[class='box-red',given='these finer red brands',answer='the firebrand',pos='0:1']||box[class='box-white',given='the mischievous',pos='1:1']||box[class='box-blue',given='he fir ran',pos='2:1']||box[class='box-navy',given='There is no happiness without suffering.',pos='1:2']",
+    },
+    "The Transparent": {
+        problem1: "box[class='box-white',given='c',answer='se e',pos='0:1']",
+        problem2: "box[class='box-white',given='crew',pos='0:0']||box[class='box-red',given='walkthrough',answer='through',pos='0:1']",
+        problem3: "box[class='box-magenta',given='article',pos='0:1']||box[class='box-red',given='theory',answer='the',pos='1:1]",
+        problem4: "box[class='box-purple',given='halls',answer='walls',pos='0:1']",
+        problem5: "box[class='box-mint',given='previous 4',pos='0:1']||box[class='box-cyan',given='nothing',answer='what do you see',pos='1:1]",
+    }
+}
--- script.js
+++ script.js
@@ -2,348 +2,245 @@ function byId(id) {
 	return document.getElementById(id);
 }
 function randItem(arr) {
-	return arr[Math.floor(Math.random()*arr.length)];
+	return arr[Math.floor(Math.random() * arr.length)];
 }
 function randNum(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-var score = 0;
-var input = byId('input');
-var scoretxt = byId('score');
-var box = byId('box');
-var submitbtn = byId('submit');
-var colors = {
-	blue: {
-		name: 'blue',
-		color: 'rgb(0,0,255)',
-		desc: 'Add the number.',
-		limit: [0,1000],
-		modlimit: [0,1000],
-	},
-	red: {
-		name: 'red',
-		color: 'rgb(255,0,0)',
-		desc: 'Subtract the number.',
-		limit: [0,1000],
-		modlimit: [0,1000],
-		mult: 1.5,
-	},
-	purple: {
-		name: 'purple',
-		color: 'rgb(100,20,120)',
-		desc: 'Multiply the number.',
-		limit: [0,500],
-		modlimit: [1,20],
-	},
-	green: {
-		name: 'green',
-		color: 'rgb(100,212,10)',
-		desc: 'Divide the number.',
-		limit: [0,500],
-		modlimit: [1,20],
-	},
-	almond: {
-		name: 'almond',
-		color: 'rgb(230,215,175)',
-		desc: 'Square root of the number.',
-		limit: [0,500],
-		modlimit: false,
-		modampmode: "nomod",
-	},
-	teal: {
-		name: 'teal',
-		color: 'rgb(0,235,190)',
-		desc: 'Mean difference with the pair. Mean of (n[0]-m) and (n[1]-m).',
-		limit: [0,500],
-		modlimit: [0,500],
-		limitmode: 2,
-	},
-	yellow: {
-		name: 'yellow',
-		color: 'rgb(255,235,50)',
-		desc: 'a^2 + b^2 = ?. Answer with c^2, not c.',
-		limit: [0,80],
-		modlimit: [0,80],
-		scoremult: 2,
-	},
-	crimson: {
-		name: 'crimson',
-		color: 'rgb(120,35,35)',
-		desc: 'Replicate a fibonacci sequence.<br>Example:n[0] = 5, n[1] = 10, m = 3. 5+10 = 15 (1 completed), 10+15 = 25 (2 completed), 15+25 = 40 (3 completed). k = 40.',
-		limit: [1,80],
-		modlimit: [2,5],
-		limitmode: 2,
-		scoremult: 2,
-	},
-	limepurple: {
-		name: 'limepurple',
-		color: 'linear-gradient(to bottom, rgb(160,255,50) 0%, rgb(160,255,50) 50%, rgb(100,20,210) 50%, rgb(100,20,210) 100%)',
-		desc: 'Find the least common multiple of n and m.',
-		limit: [1,80],
-		modlimit: [1,80],
-	},
-	limegreen: {
-		name: 'limegreen',
-		color: 'linear-gradient(to bottom, rgb(160,255,50) 0%, rgb(160,255,50) 50%,rgb(0,212,10) 50%, rgb(0,212,10) 100%)',
-		desc: 'Find the greatest common factor of n and m.',
-		limit: [1,160],
-		modlimit: [1,160],
-		scoremult: 2.5,
-	},
-	black: {
-		name: 'black',
-		color: 'rgb(0,0,0)',
-		desc: 'Find the logarithm of n with base m.',
-		limit: [1,1000],
-		modlimit: [1,10],
-		scoremult: 3,
-		css: {color: "rgb(255,255,255)",border:"5px solid rgb(100,100,100)"},
-	},
-	slate: {
-		name: 'slate',
-		color: 'rgb(85,100,130)',
-		desc: 'Find (n^2+m^2)/(n+m).',
-		limit: [1,80],
-		modlimit: [1,80],
-		scoremult: 6,
-	},
-	amber: {
-		name: 'amber',
-		color: 'rgba(255,160,50,1)',
-		desc: 'Double the digits and then move them 1 to the left.<br>Example: 94. 9->18, 4->8. 188->881.',
-		limit: [11,100],
-		modlimit: false,
-		scoremult: 1.5,
-		modampmode: "nomod",
-		css: {boxshadow:"box-shadow?4px 4px 1px 3px inset rgba(220,120,30,0.8), -4px -4px 1px 3px inset rgba(220,120,30,0.8)",},
-	},
-	gold: {
-		name: 'gold',
-		color: 'rgb(225,205,20)',
-		desc: 'Halve the number.',
-		limit: [2,5000],
-		modlimit: false,
-		scoremult: 1,
-		modampmode: "nomod",
-	},
-	inverto: {
-		name: 'inverto',
-		color: 'linear-gradient(45deg, rgb(100,20,255) 0%, rgb(100,20,255) 50%,rgb(180,255,50) 50%, rgb(180,255,50) 100%)',
-		desc: 'Find what percent of m is n.',
-		limit: [1,500],
-		modlimit: [1,500],
-		scoremult: 3,
-	},
-	crimsonamber: {
-		name: 'crimsonamber',
-		color: 'linear-gradient(to bottom,rgba(255,160,50,1) 0%, rgb(255,160,50) 50%, rgb(120,35,35) 50%, rgb(120,35,35) 100%)',
-		desc: 'Add the two digits and then rotate to the left m times.<br>Example: n[0] = 11, n[1] = 50, m = 2. 11+50 = 61, 61->16. 16+50 = 66, 66->66.',
-		limit: [11,80],
-		modlimit: [2,5],
-		scoremult: 3,
-		limitmode: 2,
-		css: {boxshadow:"box-shadow?4px 4px 1px 3px inset rgba(220,120,30,0.8), -4px -4px 1px 3px inset rgba(100,20,20,0.8)",},
-	},
+	return Math.ceil(Math.random() * (max - min)) + min - 1;
+}
+function splitTwo(str, delim1, delim2) {
+	return str.split(delim1)[1].split(delim2)[0];
+}
+function accSplitTwo(str, delim1, delim2) {
+	let amount = str.split(delim1).length - 1;
+	let amount2 = str.split(delim2).length - 1;
+}
+String.prototype.replaceAt = function (index, replacement) {
+	return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+//IMPORTANT
+var submitBtn = document.getElementById('submit');
+var submitInput = document.getElementById('answer');
+var backBtn = document.getElementById('back');
+var nextBtn = document.getElementById('next');
+var hintBtn = document.getElementById('hint');
+var eventP = byId("event-p");
+var omegaDiv = byId("omega-text");
+var omegaP = byId("omega-p");
+var omegaBtn = byId("exit-omega");
+var currentAnswer = "";
+var currentProblem = 1;
+var currentSection = 0;
+var boxes = [];
+var sub_boxes = [];
+//helpvar
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var inputFill = "-- -";
+var completed = [];
+var completedSections = [];
+var nextAvailable = false;
+
+function genAnswer(section, problem) {
+	if (boxes) {
+		currentAnswer = "";
+		boxes.forEach(function (item) {
+			item.remove();
+		});
+		sub_boxes.forEach(function (item) {
+			item.remove();
+		});
+	}
+
+	given = puzzles[section]["problem" + problem];
+	givensplit = given.split("||");
+	givensplit.forEach(function (item) {
+		if (item.substring(0, 5) == "table") {
+			let table = byId("target-div");
+			while (table.firstChild) {
+				table.removeChild(table.lastChild);
+			}
+			args = splitTwo(item, "[", "]").split(",");
+			args1 = args[0].split("=");
+			args2 = args[1].split("=");
+			zelen = 3;
+			zeheight = 3;
+			if (args1[0] == "length") {
+				zelen = args1[1];
+				zeheight = args2[1];
+			} else {
+				zelen = args2[1];
+				zeheight = args1[1];
+			}
+			for (let i =0; i < zeheight; i++) {
+				zerow = document.createElement("tr");
+				for (let j = 0; j < zelen; j++) {
+					zetd = document.createElement("td");
+					zetd.setAttribute("id",i+":"+j);
+					zerow.appendChild(zetd);
+				}
+				table.appendChild(zerow);
+			}
+		}
+		if (item.substring(0, 3) == "box") {
+			box = document.createElement("div");
+			box.classList.add("normal-box");
+			sub_box = document.createElement("div");
+			sub_box.classList.add("sub-box");
+			para = document.createElement("p");
+			zeattributes = splitTwo(item, "[", "]").split(",");
+			position = "";
+			zeattributes.forEach(function (item2) {
+				arg1 = item2.split("=")[0];
+				arg2 = item2.split("=")[1];
+				if (arg1 == "class") {
+					box.classList.add(splitTwo(arg2, "'", "'"));
+				}
+				if (arg1 == "given") {
+					para.innerHTML = splitTwo(arg2, "'", "'");
+				}
+				if (arg1 == "answer") {
+					currentAnswer = splitTwo(arg2, "'", "'");
+				}
+				if (arg1 == "pos") {
+					position = splitTwo(arg2, "'", "'");
+				}
+			});
+			byId(position).appendChild(box);
+			box.appendChild(sub_box);
+			sub_box.appendChild(para);
+			boxes.push(box);
+			sub_boxes.push(sub_box);
+		}
+	});
+	inputFill = "";
+	for (let i = 0; i < currentAnswer.length; i++) {
+		if (currentAnswer.charAt(i) != " ") {
+			inputFill += "-";
+		} else {
+			inputFill += " ";
+		}
+	}
+	submitInput.value = inputFill;
+}
+genAnswer("The Transparent", 1);
+function clearP() {
+	eventP.innerHTML = "";
+}
+function checkInput() {
+	let curvalue = submitInput.value;
+	let result = inputFill.split('');
+	let cursorPosition = submitInput.selectionStart;
+
+	// Replace dashes with user input characters
+	for (let i = 0, j = 0; i < result.length && j < curvalue.length; i++) {
+		if ((result[i] === '-' && curvalue[j] !== ' ') || (i == 0 && result[i] == currentAnswer.charAt(0))) {
+			result[i] = curvalue[j];
+			j++;
+
+		} else if (result[i] === ' ') {
+			if (curvalue[j] != '-') {
+				result[i] = curvalue[j];
+				j++;
+			}
+			continue;
+		} else if (curvalue[j] === ' ') {
+			// Skip over spaces in the user input
+			j++;
+			i--; // Recheck the current position in the next loop iteration
+		}
+	}
+
+	// Join the array back into a string
+	result = result.join('');
+
+	// Update the input value and set the cursor position
+	submitInput.value = result;
+	submitInput.setSelectionRange(cursorPosition, cursorPosition);
+
 };
-var correct = 0;
-var correctanswer = 0;
-var num = 0;
-var mod = 0;
-var inputval = 0;
-var numtxt = byId("num");
-var modtxt = byId("mod");
-var desctxt = byId("desctxt");
-var currentcolor;
-var scoreinc;
-function gcd(a, b) { 
-    for (let temp = b; b !== 0;) { 
-        b = a % b; 
-        a = temp; 
-        temp = b; 
-    } 
-    return a; 
-} 
-  
-function lcm(a, b) { 
-    let gcdValue = gcd(a, b); 
-    return (a * b) / gcdValue; 
-}
-function logb(log, base) {
-	return Math.log(log)/Math.log(base); 
-}
-function numDigits(n) {
-	let cnt = 0;
-	while (n > 0) {
-		cnt++;
-		n = parseInt(n / 10, 10);
-	}
-	return cnt;
-}
-function leftShift(num,iterations) {
-	var temp  = String(num);
-	for( let i = 0; i <iterations; i++){
-		temp = temp.substring(1)+temp.substring(0,1);
-	}
-	return Number(temp);
-}
-function doubleDigits(num) {
-	let digits = numDigits(num);
-	let tempstr = "";
-	let numstr = String(num);
-
-	for (let i = 0; i < digits; i++) {
-		let zenum = Number(numstr.substring(i,i+1))*2;
-		tempstr += zenum;
-	}
-	return Number(tempstr);
-}
-function newbox() {
-	let newbox = colors[randItem(Object.keys(colors))];
-	box.style.background = newbox.color;
-	currentcolor = newbox.name;
-	if (Object.hasOwn(newbox,"css")) {
-		for (let i =0; i < Object.keys(newbox.css).length; i++) {
-			let tempcss = newbox.css[Object.keys(newbox.css)[i]];
-			let realprop = Object.keys(newbox.css)[i];
-			if (tempcss.includes("?")) {
-				realprop = tempcss.split("?")[0];
-				tempcss = tempcss.split("?")[1];
-			}
-			box.style[realprop] = tempcss;
+submitBtn.addEventListener("click", function () {
+	curvalue = submitInput.value;
+	if (curvalue == currentAnswer) {
+		eventP.innerHTML = "<b style='color:green;'>Correct Answer!</b>";
+		completedCode = currentSection + ":" + currentProblem;
+		if (completed.includes(completedCode) == false) {
+			completed.push(completedCode);
 		}
+		nextBtn.classList.remove("hide");
 	} else {
-		box.style.color = "rgb(0,0,0)";
-		box.style.border = "5px solid black";
-		box.style.boxShadow = "none";
+		eventP.innerHTML = "<b style='color:red;'>Wrong Answer!</b>";
 	}
-	if (Object.hasOwn(newbox,"limitmode")) {
-		if (isNaN(newbox.limitmode) == false) {
-			num = [];
-			for (let i =0; i < newbox.limitmode; i++) {
-				num.push(randNum(newbox.limit[0],newbox.limit[1]));
-			}
-		}
-	} else {
-		num = randNum(newbox.limit[0],newbox.limit[1]);
-	}
-	
-	if (Object.hasOwn(newbox,"modampmode")) {
-		if (newbox.modampmode == "nomod") {
-			mod = "";
-		}	
-		if (newbox.modampmode == "mult") {
-			let modamp = randNum(newbox.modlimit[0],newbox.modlimit[1]);
-			mod = Math.round(num*modamp);
-		}	
-	} else {
-		mod = randNum(newbox.modlimit[0],newbox.modlimit[1]);
-	}
-	
-	
-	input.value = "";
-	if (newbox.name == 'blue') {
-		correctanswer = num+mod;
-	}
-	if (newbox.name == 'red') {
-		correctanswer = num-mod;
-	}
-	if (newbox.name == 'purple') {
-		correctanswer = num*mod;
-	}
-	if (newbox.name == 'green') {
-		correctanswer = Math.round(num/mod);
-	}
-	if (newbox.name == 'almond') {
-		correctanswer = Math.round(Math.sqrt(num));
-	}
-	if (newbox.name == 'teal') {
-		correctanswer = Math.round((Math.abs(num[0]-mod)+Math.abs(num[1]-mod))/2);
-	}
-	if (newbox.name == 'yellow') {
-		correctanswer = Math.pow(num,2)+Math.pow(mod,2);
-	}
-	if (newbox.name == 'crimson') {
-		if (num[0] > num[1]) {
-			num[1] = num[0]+num[1];
-		}
-		let newnum = num[1];
-		let lastnum = num[0];
-		let newnum2 = 0;
-		for (let i =0; i < mod; i++) {
-			newnum2 = newnum;
-			newnum = newnum+lastnum;
-			lastnum = newnum2;
-		}
-		correctanswer = newnum;
-
-	}
-	if (newbox.name == 'limepurple') {
-		correctanswer = lcm(num,mod);
-	}
-	if (newbox.name == 'limegreen') {
-		correctanswer = gcd(num,mod);
-	}
-	if (newbox.name == 'black') {
-		correctanswer = Math.round(logb(num,mod));
-	}
-	if (newbox.name == 'slate') {
-		correctanswer = Math.round((Math.pow(num,2)+Math.pow(mod,2))/(num+mod));
-	}
-	if (newbox.name == 'amber') {
-		correctanswer = leftShift(doubleDigits(num),1);
-	}
-	if (newbox.name == 'gold') {
-		correctanswer = Math.round(num/2);
-	}
-	if (newbox.name == 'inverto') {
-		if (num > mod) {
-			mod = num+mod;
-		}
-		correctanswer = Math.round((num/mod)*100);
-	}
-	if (newbox.name == 'crimsonamber') {
-		if (num[0] > num[1]) {
-			num[1] = num[0]+num[1];
-		}
-		let newnum = num[1];
-		let lastnum = num[0];
-		let newnum2 = 0;
-		for (let i =0; i < mod; i++) {
-			newnum2 = newnum;
-			newnum = leftShift(newnum+lastnum,1);
-			lastnum = newnum2;
-		}
-		correctanswer = newnum;
-	}
-
-	scoreinc = Math.max(Math.round(Math.log(Math.abs(correctanswer))/1.5),1);
-	if (Object.hasOwn(newbox,"scoremult")) {
-		scoreinc *= newbox.scoremult;
-	}
-
-	//text
-	numtxt.innerHTML = num;
-	modtxt.innerHTML = mod;
-	desctxt.innerHTML = newbox.desc;
-	//
-	correctanswer = String(correctanswer);
-	score += scoreinc;
-}
-function update() {
-	scoretxt.innerHTML = "Score: "+score;
-	inputval = input.value;
-	if (inputval == correctanswer) {
-		newbox()
-	} else {
-		window.alert("WRONG!");
-	}
-}
-update();
-submitbtn.addEventListener('click',function() {
-	update();
+	setTimeout(clearP, 2000);
 });
-function endScore() {
-	scoretxt.innerHTML = "Game over! End Score: "+score;
-	submitbtn.style.display = "none";
-}
-window.setTimeout(endScore,60000);
+backBtn.addEventListener("click", function() {
+	if (currentProblem > 1) {
+		currentProblem -= 1;
+		nextBtn.classList.remove("hide");
+		genAnswer(Object.keys(puzzles)[currentSection], currentProblem);
+		if (currentProblem == 1) {
+			backBtn.classList.add("hide");
+		}
+	}
+});
+nextBtn.addEventListener("click", function() {
+	next = puzzles[Object.keys(puzzles)[currentSection]]["problem"+(currentProblem+1)];
+	completedCode = currentSection + ":" + (currentProblem);
+	if (completed.includes(completedCode)) {
+		currentProblem += 1;
+		backBtn.classList.remove("hide");
+		if (currentProblem - 1 == Object.keys(puzzles[Object.keys(puzzles)[currentSection]]).length) {
+			omegaDiv.classList.remove("hide");
+			omegaP.innerHTML = Object.keys(puzzles)[currentSection].toUpperCase();
+			document.querySelectorAll("[data-level='"+Object.keys(puzzles)[currentSection]+"']").forEach(function(element) {
+				element.innerHTML = currentSection + ". " + Object.keys(puzzles)[currentSection];
+			});
+			if (completedSections.includes(currentSection) == false) {
+				completedSections.push(currentSection);
+			}
+			currentSection = 0;
+			currentProblem = 1;
+			byId("problems").classList.add("hide");
+			backBtn.classList.add("hide");
+			nextBtn.classList.add("hide");
+			
+		} else {
+			genAnswer(Object.keys(puzzles)[currentSection], currentProblem);
+			completedCode2 = currentSection + ":" + (currentProblem);
+			if (completed.includes(completedCode2) == false) {
+				nextBtn.classList.add("hide");
+			} else {
+				nextBtn.classList.remove("hide");
+			}
+		}
+		
+	}
+});
+hintBtn.addEventListener("click", function() {
+	if (inputFill.charAt(0) == "-") {
+		inputFill = currentAnswer.charAt(0) + inputFill.substring(1);
+	}
+	submitInput.value = inputFill;
+	
+});
+document.querySelectorAll(".level-link").forEach((element) => {
+	element.addEventListener("click", () => {
+		level = element.getAttribute("data-level");
+		byId("problems").classList.remove("hide");
+		if (isNaN(level)) {
+			genAnswer(level,1);
+			for (let i = 0; i < Object.keys(puzzles).length; i++) {
+				if (Object.keys(puzzles)[i] == level) {
+					currentSection = i;
+				}
+			}
+		} else {
+			genAnswer(Object.keys(puzzles)[Number(level)],1);
+			currentSection = level;
+		}
+		backBtn.classList.add("hide");
+		
+	});
+});
+omegaBtn.addEventListener("click", () => {
+	omegaDiv.classList.add("hide");
+});
